44f8cd975a176aeccd0406fa2ff36c92ea10c47d
dewank07 <dewankrastogi007@gmail.com>
2023-11-02 03:37:21 +0530

    Intial commit: File upload

---
+++ app/qrmaker/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import QRCode1 from "react-qr-code";
+import QRCode from "qrcode";
+import { CanvasHTMLAttributes, useEffect, useState } from "react";
+export default function QrGenerator() {
+  const [src, setSrc] = useState<any>("");
+  const [valueInput, setValueInput] = useState<string>("");
+
+  const handleChange = (e: any) => {
+    setValueInput(e.target.value);
+  };
+  const handleClick = () => {
+    QRCode.toCanvas(
+      valueInput,
+      { errorCorrectionLevel: "H" },
+      function (err, canvas) {
+        if (err) throw err;
+
+        let container = document.getElementById("container") as HTMLElement;
+        canvas.id = "qrID";
+        container.appendChild(canvas);
+      }
+    );
+    let link = document.createElement("a");
+    link.download = "qr.png";
+    link.href = (
+      document.getElementById("qrID") as HTMLCanvasElement
+    ).toDataURL("image/png");
+    link.click();
+  };
+
+  return (
+    <div className='w-full h-full flex flex-col gap-10 items-center'>
+      <h1 className='text-4xl font-semibold font-sans'>Generate your QR</h1>
+      <QRCode1 value={valueInput} id='qrCodeDown' />
+      <input
+        className='border-slate-950 border rounded-md h-10 w-72 font-medium p-4'
+        value={valueInput}
+        onChange={(e) => {
+          handleChange(e);
+        }}
+      />
+      <button
+        className='bg-orange-600 w-28 h-8 rounded-lg text-white'
+        onClick={handleClick}
+      >
+        download
+      </button>
+      <div className='hidden' id='container'></div>
+    </div>
+  );
+}
---
+++ components/navbar.tsx
@@ -0,0 +1,134 @@
+"use client";
+// imports
+import Image from "next/image";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { BsGithub } from "react-icons/bs";
+import { GrMenu } from "react-icons/gr";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export default function Navbar({}): any {
+  const { setTheme } = useTheme();
+  return (
+    <nav className='w-full backdrop-blur-md bg-white dark:bg-gray-900 bg-opacity-30 z-50 fixed h-24 flex justify-between items-center py-10 px-4 md:px-8 lg:px-12 xl:px-16 2xl:px-24'>
+      <Link
+        href='/'
+        className='flex flex-col gap-2 items-center justify-center'
+      >
+        <div className='font-semibold text-orange-600'>
+          <div className='text-lg'>Dew Tools.</div>
+        </div>
+      </Link>
+      <div className='gap-1 hidden md:flex md:gap-2 lg:gap-4 '>
+        <Button variant='ghost' className='font-semibold text-md'>
+          <Link href='/'>Home</Link>
+        </Button>
+        <Link href='/converter'>
+          <Button variant='ghost' className='font-semibold text-md'>
+            Converter
+          </Button>
+        </Link>
+        <Link href='/scanner'>
+          <Button variant='ghost' className='font-semibold text-md'>
+            Scanner
+          </Button>
+        </Link>
+        <Link href='/qrmaker'>
+          <Button variant='ghost' className='font-semibold text-md'>
+            QR Generator
+          </Button>
+        </Link>
+      </div>
+      <div className='gap-1 hidden md:flex md:gap-2 lg:gap-4 '>
+        <Link href='https://github.com/dewank07'>
+          <Button
+            variant='default'
+            className='rounded-full w-fit bg-orange-600 gap-2 items-center hidden md:flex '
+            size='lg'
+          >
+            <span>Know the Authour</span>
+            <span className='text-xl'>
+              <BsGithub />
+            </span>
+          </Button>
+        </Link>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='outline' size='icon'>
+              <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+              <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+              <span className='sr-only'>Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+
+      {/* MOBILE NAV */}
+      <Sheet>
+        <SheetTrigger className='block md:hidden p-3'>
+          <span className='text-2xl'>
+            <GrMenu />
+          </span>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetDescription>
+              <div className='w-full space-y-3'>
+                <Link href='/'>
+                  <Button
+                    variant='link'
+                    className='font-semibold text-md w-full'
+                  >
+                    Home
+                  </Button>
+                </Link>
+                <Link href='/about'>
+                  <Button
+                    variant='link'
+                    className='font-semibold text-md w-full'
+                  >
+                    About
+                  </Button>
+                </Link>
+                <Link href='/privacy-policy'>
+                  <Button
+                    variant='link'
+                    className='font-semibold text-md w-full'
+                  >
+                    Privacy Policy
+                  </Button>
+                </Link>
+              </div>
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
+}
