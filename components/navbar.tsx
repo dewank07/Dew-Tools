58b93db5a189b072454be8d664491a4ad7ae0896
dewank07 <dewankrastogi007@gmail.com>
2023-11-05 02:51:24 +0530

    fix: mobile view navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -128,6 +128,33 @@ export default function Navbar({}): any {
                     </SheetClose>
                   </Button>
                 </SheetClose>
+                <SheetClose asChild>
+                  <Button variant='ghost' className='font-semibold text-md'>
+                    <SheetClose asChild>
+                      <Link href='/notion'>Note-ion</Link>
+                    </SheetClose>
+                  </Button>
+                </SheetClose>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant='outline' size='icon'>
+                      <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+                      <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+                      <span className='sr-only'>Toggle theme</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align='end'>
+                    <DropdownMenuItem onClick={() => setTheme("light")}>
+                      Light
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("dark")}>
+                      Dark
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => setTheme("system")}>
+                      System
+                    </DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
               </div>
             </SheetDescription>
           </SheetHeader>
